7c18f0bd88bfb80ea287d6e8a40b58028327807a
Я <koalder@yandex.ru>
2024-09-19 13:16:04 +0300

    Написаны pytest-ы для грязных и чистых функций классов TestMenu и TestPlayer, требующие запуска в терминале в виде pytest -s

--- Loto_test.py
+++ Loto_test.py
@@ -1,9 +1,21 @@
+# Из-за input() запускать в терминале так: pytest -s
 import pytest
-from Loto_class import Players, Card, Barrels, Interface
+from Loto_class import Menu, Players, Card, Barrels, Interface
+class TestMenu:
+    def test_menu(self): # Не число ли и вне пределов?
+        menu = Menu().start_menu()
+        assert menu in [1, 2, 3, 4] # Для проверки нужно ввести число от 1 до 4
+
 class TestPlayer:
-    def test_players(self):
-        players = Players(['Первый игрок', 'Второй игрок'])
-        assert len(players.players) == 2 and players.players == ['Первый игрок', 'Второй игрок']
+    def test_two_players(self): # Проверяет количество пар игроков в списке
+        players = Players()
+        assert len(players.two_players) == 3
+    def test_players_list(self): # Список ли получает?
+        players = Players().add_players()
+        assert type(players) == list
+    def multi_players_zero(self): # Срабатывает ли при введении числа игрогов больше нуля
+        players = Players().multi_players()
+        assert len(players) > 0 # Для проверки нужно ввести число больше нуля
 
 class TestCard:
     pass
