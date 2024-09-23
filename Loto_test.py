f345c7028d84d8497eb6f3bf075449f7399677d8
AldeR <koalder@yandex.ru>
2024-09-23 20:53:33 +0300

    Создал тест для класса Barrels и для инициатора класса Interface

--- Loto_test.py
+++ Loto_test.py
@@ -29,11 +29,35 @@ class TestCard:
         assert len(card_lst) == 3 # Три ли строчки в карточке?
         for str_lst in card_lst: # Перебираем строчки (вложенные листы) в карточке
             assert len(str_lst) == 9 # 9 ли элементов в строчке (во вложенных листах) карточки?
-            assert str_lst.count('') == 4 # В них есть ли 4 пустых строк?
+            assert str_lst.count('') == 4 # Есть ли в них 4 пустые строки?
             assert len(set([str_lst[i] for i in range(len(str_lst))])) > 2 # Рандомно ли перебираются цифры в этих строчках?
+
 class TestBarrels:
-    pass
+    def test_create_barrels_list(self): # Для проверки создания списка с 90 бочонками
+        barrels_lst = Barrels().create_barrels_list() # Cоздаём экземпляр метода класса
+        assert len(barrels_lst) == 90 # В листе 90 элементов (бочонков)?
+        assert len(set(barrels_lst)) == len(barrels_lst) # Они разные?
+
 class TestInterface:
-    pass
+    def test_interface(self): # Для проверки инициированных переменных
+        card_lst = Card().create_card_list() # Cоздаём экземпляр метода класса c картами
+        barrels_lst = Barrels().create_barrels_list() # А теперь экземпляр метода класса с бочонками
+        interface = Interface(['Игрок_1', 'Игрок_2'], card_lst, barrels_lst) # И объект предитогового класса
+
+        assert interface.players == ['Игрок_1', 'Игрок_2'] # Сохраняет ли неизменно в переменную поданный лист?
+
+        # Ни дескрипторы, ни общий класс почему-то не помогли, пока что оставим дублированными ... на следующий раз
+        assert len(card_lst) == 3 # Три ли строчки в переданной карточке?
+        for str_lst in card_lst: # Перебираем строчки (вложенные листы) в переданной карточке
+            assert len(str_lst) == 9 # 9 ли элементов в строчке (во вложенных листах) этой карточки?
+            assert str_lst.count('') == 4 # Есть ли в них 4 пустые строки?
+            assert len(set([str_lst[i] for i in range(len(str_lst))])) > 2 # Рандомно ли перебираются цифры в этих строчках?
+
+        # Такая же ситуёвина
+        assert len(barrels_lst) == 90 # В переданном листе 90 элементов (бочонков)?
+        assert len(set(barrels_lst)) == len(barrels_lst) # Они разные?
+
+        assert len(interface.players) == len(interface.count_dict) # Совпадают ли количество игроков в словаре и списке?
+        assert interface.players == [i for i, _ in interface.count_dict.items()] # Одинаковые ли имена игроков в списке и словаре?
 class TestGame:
     pass
